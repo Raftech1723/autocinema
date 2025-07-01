a612acf617e29d75933ac4002c3b6744a3b45838
eorbezom <eorbezo99@gmail.com>
2025-06-30 23:55:38 -0500

    se soluciona la seccion de pelicula

--- autocinema-pelicula/src/main/java/com/pelicula/controller/PeliculaController.java
+++ autocinema-pelicula/src/main/java/com/pelicula/controller/PeliculaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.categoria.service.CategoriaService;
 import com.pelicula.entity.Pelicula;
 import com.pelicula.repository.PeliculaRepository;
 import com.pelicula.service.PeliculaService;
